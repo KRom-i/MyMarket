66dc3a35c53973d72b312c6a7f99bfab3c7a5455
KRom-i <emailroma@mail.ru>
2021-10-18 14:36:52 +0300

    Home_work_1

--- src/main/java/com/gb/market/controllers/AdminController.java
+++ src/main/java/com/gb/market/controllers/AdminController.java
@@ -1,17 +1,57 @@
 package com.gb.market.controllers;
 
-
+import com.gb.market.entities.market.Category;
+import com.gb.market.entities.market.Product;
+import com.gb.market.entities.user.SystemUser;
+import com.gb.market.services.CategoryService;
+import com.gb.market.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("")
     public String adminHome() {
         return "admin";
+    }
+
+    @GetMapping("/productForm")
+    public String formProduct(Model model) {
+
+        model.addAttribute ("product", new Product ());
+        model.addAttribute ("categories", productService.getAllCategories ());
+        model.addAttribute ("productsAdded", productService.getProductsLastInSize (10));
+
+        return "product_form";
+    }
+
+    @PostMapping("/addNewProduct")
+    public String addNewProduct(Model model, @ModelAttribute("product") Product saveProduct) {
+
+        if (productService.save(saveProduct)){
+            model.addAttribute ("message", "Товар добавлен в базу данных");
+            model.addAttribute ("product", new Product ());
+
+        } else {
+            model.addAttribute ("error", "Ошибка при сохранении товара");
+            model.addAttribute ("product", saveProduct);
+
+        }
+
+        model.addAttribute ("categories", productService.getAllCategories ());
+        model.addAttribute ("productsAdded", productService.getProductsLastInSize (10));
+
+        return "product_form";
     }
 
 
--- src/main/java/com/gb/market/repositories/ProductRepository.java
+++ src/main/java/com/gb/market/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.gb.market.repositories;
 
 import com.gb.market.entities.market.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>,
         JpaSpecificationExecutor<Product> {
+
+    Product findOneByTitle(String title);
+
+    Product findTopByOrderByIdDesc();
+
+    Page<Product> findByOrderByIdDesc (Pageable pageable);
 
 }
